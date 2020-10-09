ff4d75c6e422ae6134ecc7f2ee86282613111135
Jakub Sirový <38387772+jakubsirovy@users.noreply.github.com>
2020-10-09 18:01:42 +0200

    Initial commit

---
+++ lib/api.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Api {
+  String url = "https://pavel-vacha.cz/albistech/";
+  load(String module, {var params}) async {
+    var response = await http.post(url + module);
+    return json.decode(response.body);
+  }
+}
